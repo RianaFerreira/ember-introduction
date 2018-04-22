ea3d213def0c7002d83d35ee3fb84d0b2a67aaf7
Riana Ferreira <rianaferreira@gmail.com>
2018-04-22 14:49:48 +1000

    Introduction to routing

--- app/router.js
+++ app/router.js
@@ -7,6 +7,16 @@ const Router = EmberRouter.extend({
 });
 
 Router.map(function() {
+  this.route('about', {path: 'about'});   // Don't need to specify the path if it's named the same as the route
+  this.route('services');
+  this.route('contact');
+  this.route('events');
+
+  // Nested route
+  this.route('posts', {path: '/posts'}, function() {
+    this.route('new');
+    this.route('post', {path: '/post_id'});
+  })
 });
 
 export default Router;
